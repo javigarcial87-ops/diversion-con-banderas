5d240380067bc58709b766e2d6b9bf0f1c4fcb19
javier garcia alvarez <javigarcial87@gmail.com>
2025-11-25 20:43:17 +0100

    funcion que trae un objeto con la informacion

--- js/script.js
+++ js/script.js
@@ -4,8 +4,19 @@ console.log('Empezamos!!')
 const url = 'https://restcountries.com/v3.1/all?fields=name,flags,car,population,capital'
 
 
+async function infoPaises (){
 
+   try{
+    response = await fetch (url)
+    data= await response.json()
+    return data
+   } catch (error) {
+    mostrar(error)
+    return nada
+   }
+}
 
+infoPaises().then(data => console.log(data))
 
 
 
