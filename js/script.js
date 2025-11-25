4d16a141f1ff270e82855c0c1aeef2b476b1f248
javier garcia alvarez <javigarcial87@gmail.com>
2025-11-25 21:36:08 +0100

    cambios js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,51 @@
 console.log('Empezamos!!')
 
-
+const paisesContainer = document.getElementById("paises-container")
 const url = 'https://restcountries.com/v3.1/all?fields=name,flags,car,population,capital'
 
 
 async function infoPaises (){
 
    try{
-    response = await fetch (url)
-    data= await response.json()
+    const response = await fetch (url)
+    const data= await response.json()
     return data
    } catch (error) {
-    mostrar(error)
-    return nada
+    console.log(error)
+    return null
    }
 }
 
-infoPaises().then(data => console.log(data))
+infoPaises().then(data => console.log('INFOPAISES: ',data))
 
+function createCountryCard(){
+    const card = document.createElement('div')
+    card.classList.add('card')
+    const img= document.createElement('img')
+    img.src= pais.flags.png
+    img.alt="bandera de" + pais.name.common
+    const titulo = document.createElement('h3')
+    titulo.textContent=pais.name.common
+    card.appendChild(img)
+    card.appendChild(titulo)
+    card.addEventListener('click',()=>openModal(pais))
+    return card
+}
+
+
+
+
+
+
+
+function renderCountries() {
+    paisesContainer.innerHTML=""
+    paises.forEach(pais => {
+        
+        paisesContainer.appendChild
+
+    });
+}
 
 
 
